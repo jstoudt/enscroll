b7091cebd6690c728e0ec17e53db426f24280f49
Jason Stoudt <jstoudt98@gmail.com>
2011-12-31 15:27:09 -0500

    Initial commit

---
+++ enscroll.js
@@ -0,0 +1,234 @@
+/*
+ * enscroll.js
+ */
+
+(function($) {
+
+	$.fn.enscroll = function(opts) {
+
+		// use default settings, and overwrite defaults with options passed in
+		var settings = $.extend({
+			trackWidth: 16,
+			scrollIncrement: 20,
+			trackClass: 'track',
+			handleClass: 'handle'
+		}, opts),
+
+			mouseYOffset, // y-offset of mouse-down on the handle
+
+			handle, // a reference to the handle element
+
+			handleY, // the current y-offset position of the handle
+
+			handleHeight, // the height of the handle
+
+			trackHeight, // the height of the track
+
+			trackYOffset, // y-offset of the track relative to the document
+
+			pane, // a reference to the target pane
+
+			paneScrollHeight, // the total height of the target pane
+
+			touchY, // the document-relative y-coordinate of a touch event
+
+			dragging = false, // true when the mouse button is depressed
+
+			eventUtility = {
+				
+				getEvent: function(event) {
+					return event ? event : window.event;
+				},
+
+				preventDefault: function(event) {
+					if (event.preventDefault) {
+						event.preventDefault();
+					} else {
+						event.returnValue = false;
+					}
+				}
+
+			},
+			startDrag = function(event) {
+				// only enScroll for left mouse button down events
+				if (event.which !== 1) {
+					return true;
+				}
+				handle = event.target;
+				handleY = parseInt(handle.style.top, 10);
+				var track = handle.parentNode,
+					that = this;
+				pane = event.data.pane;
+				paneScrollHeight = pane.scrollHeight;
+				mouseYOffset = event.clientY - Math.round($(handle).offset().top);
+				handleHeight = $(handle).height();
+				trackHeight = $(track).height();
+				trackYOffset = $(track).offset().top;
+				dragging = true;
+				$(document.body).mousemove(moveDrag).mouseup(function(event) {
+					endDrag.call(that, event)
+				});
+				(function moveHandle() {
+					if (dragging === true) {
+						handle.style.top = handleY + 'px';
+						$(pane).scrollTop((handleY / (trackHeight - handleHeight)) *
+							(paneScrollHeight - trackHeight));
+						setTimeout(moveHandle, 25);
+					}
+				})();
+				$(this).css('cursor', 'ns-resize');
+				return false;
+			},
+
+			moveDrag = function(event) {
+				if (dragging !== true) {
+					return false;
+				}
+				handleY = event.clientY - trackYOffset - mouseYOffset;
+				handleY = Math.max(0, Math.min(handleY, trackHeight - handleHeight));
+				return false;
+			},
+
+			endDrag = function(event) {
+				$(this).css('cursor', 'pointer');
+				dragging = false;
+				$(document.body).unbind('mousemove', moveDrag).unbind('mouseup', endDrag);
+				return false;
+			},
+
+			mouseScroll = function(event, scrollIncrement) {
+				var delta = (event.detail) ? event.detail * -40 :
+					(typeof client !== 'undefined' && client.engine.opera && client.engine.opera < 9.5) ? -event.wheelDelta :
+					event.wheelDelta;
+				
+				if (delta < 0) {
+					this.scrollTop(this.scrollTop() + scrollIncrement);
+				} else {
+					this.scrollTop(this.scrollTop() - scrollIncrement);
+				}
+				eventUtility.preventDefault(event);
+			},
+
+			paneScrolled = function(event) {
+				var handle = event.data.handle,
+					track = event.data.track,
+					pct = $(this).scrollTop() / (this.scrollHeight - $(this).height());
+				$(handle).css('top', (pct * ($(track).height() - $(handle).height())) + 'px');
+			},
+
+			positionTrack = function(pane, track) {
+				var offset = $(pane).offset(),
+					x = Math.round(offset.left) + $(pane).outerWidth() - settings.trackWidth - parseInt($(pane).css('border-right-width'), 10),
+					y = Math.round(offset.top) + parseInt($(pane).css('border-top-width'), 10);
+
+				$(track).css({
+					'left': x + 'px',
+					'top': y + 'px'
+				});
+			},
+
+			resizeHandle = function(pane, trackWrapper) {
+				var pct = $(pane).height() / pane.scrollHeight,
+					track = trackWrapper.children[0],
+					handle = track.children[0],
+					handleHeight = Math.round(Math.max(pct * $(track).height(), 25)),
+					event = {
+						data: {
+							track: track,
+							handle: handle
+						}
+					};
+				handle.style.height = handleHeight + 'px';
+				trackWrapper.style.display = (pct < 1) ? 'block' : 'none';
+				paneScrolled.call(pane, event);
+			},
+
+			touchStart = function(event) {
+				event = eventUtility.getEvent(event);
+				touchY = event.touches[0].clientY;
+				eventUtility.eventDefault(event);
+			},
+
+			touchMove = function(event) {
+				event = eventUtility.getEvent(event);
+				var touchY0 = touchY;
+				touchY = event.touches[0].clientY;
+				$(this).scrollTop($(this).scrollTop() + (touchY0 - touchY));
+				eventUtility.preventDefault(event);
+			};
+		
+		return this.each(function() {
+
+			var $this = $(this),
+				pane = $this.get(0),
+				paneHeight = $this.height(),
+				paneScrollHeight = pane.scrollHeight,
+				parent = $this.parentNode,
+				trackWrapper = document.createElement('div'),
+				track = document.createElement('div'),
+				handle = document.createElement('span'),
+				bindMouseScroll = function(event) {
+					event = eventUtility.getEvent(event);
+					mouseScroll.call($this, event, settings.scrollIncrement);
+				};
+
+			$this.css({
+				'width': ($this.width() - settings.trackWidth) + 'px',
+				'padding-right': (parseInt($this.css('padding-right'), 10) + settings.trackWidth) + 'px',
+				'overflow': 'hidden'
+			}).scroll(function(event) {
+				event.data = event.data || {};
+				event.data.track = track;
+				event.data.handle = handle;
+				paneScrolled.call(this, event);
+			});
+
+			$(window).resize(function() {
+				positionTrack(pane, trackWrapper);
+			});
+			positionTrack(pane, trackWrapper);
+
+			if (pane.addEventListener) {
+				pane.addEventListener('mousewheel', bindMouseScroll, false);
+				pane.addEventListener('DOMMouseScroll', bindMouseScroll, false);
+				pane.addEventListener('touchstart', touchStart, false);
+				pane.addEventListener('touchmove', touchMove, false);
+			} else if (pane.attachEvent) {
+				pane.attachEvent('onmousewheel', bindMouseScroll);
+			}
+
+			$(track).addClass(setTimeout.trackClass).css({
+				'position': 'relative',
+				'height': paneHeight + 'px'
+			}).appendTo(trackWrapper);
+
+			$(handle).addClass(settings.handleClass).css({
+				'display': 'block',
+				'width': '100%',
+				'position': 'absolute',
+				'left': 0,
+				'top': 0,
+				'cursor': 'pointer'
+			})
+			.appendTo(track)
+			.mousedown({pane: pane}, startDrag);
+
+			$(trackWrapper).css({
+				'position': 'absolute',
+				'width': settings.trackWidth + 'px',
+				'display': 'none'
+			}).insertAfter(this);
+
+			setInterval(function() {
+				var sh = pane.scrollHeight;
+				if (sh !== paneScrollHeight) {
+					paneScrollHeight = sh;
+					resizeHandle(pane, trackWrapper);
+				}
+			}, 300);
+
+			resizeHandle(pane, trackWrapper);
+
+		});
+	}
+})(jQuery);
