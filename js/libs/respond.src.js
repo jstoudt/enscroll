34019ee01cad9a5023eac785677d5452064c5486
Jason Stoudt <jstoudt98@gmail.com>
2012-10-18 23:27:13 -0400

    Remove copy of respond.js

--- js/libs/respond.src.js
+++ js/libs/respond.src.js
@@ -1,6 +1,5 @@
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 /*! NOTE: If you're already including a window.matchMedia polyfill via Modernizr or otherwise, you don't need this part */
-/*
 window.matchMedia = window.matchMedia || (function(doc, undefined){
   
   var bool,
@@ -26,13 +25,8 @@ window.matchMedia = window.matchMedia || (function(doc, undefined){
   };
   
 })(document);
-*/
 
 
-//======================================================================
-// --------------------- WARNING!! -------------------------------------
-//      THE CODE ABOVE DOES NOT WORK WITH IE8!
-//======================================================================
 
 
 /*! Respond.js v1.1.0: min/max-width media query polyfill. (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
